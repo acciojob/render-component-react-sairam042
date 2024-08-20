3983f9dc8bc0aad86a654dcf798e02eac905e716
sairam042 <140315270+sairam042@users.noreply.github.com>
2024-08-20 08:46:42 +0530

    first-commit

---
+++ src/components/HelloReact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HelloReact() {
+  return (
+    <div id="root">
+        <p><h1>I am learning React. My life is getting better.</h1></p>
+    </div>
+  )
+}
+
+export default HelloReact
